580aae310ce1d27450e13eaeca07c9e2e36d47df
Subrata34 <subratamodak3491@gmail.com>
2021-10-05 18:26:19 +0600

    files

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Course from './Course/Course';
-import Footer from './Footer/Footer';
+
 
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
         <div>
             
             <Course></Course>
-            <Footer></Footer>
+            
         </div>
     );
 };
